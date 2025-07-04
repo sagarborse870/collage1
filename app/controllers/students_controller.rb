fb44b560a5f3438362beb5191adcc62493df6166
sagarborse870 <sagarborse870@gmail.com>
2025-07-04 15:44:16 +0530

    Changes in create method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,49 @@
-class StudentsController < ApplicationController
+class TeachersController < ApplicationController
+  
   def index
+    @teachers=Teacher.all
+
   end
 
   def show
+    @teacher=Teacher.find(params[:id])
+  end
+
+  def view
+    @teacher=Teacher.find(params[:id])
   end
 
   def new
+    @teacher=Teacher.new()
   end
+
+  def create
+    @teacher=Teacher.new(teacher_params)
+    if @teacher.save
+      redirect_to @teacher
+    else
+      render :new
+    end
+  end
+
 
   def edit
+    @teacher=Teacher.find(params[:id])
   end
 
-  def delete
+  def update
+    @teacher=Teacher.find(params[:id])
+    if @teacher.update(teacher_params)
+      redirect_to @teacher
+    else
+      render :edit
+    end
   end
+
+    private
+    def teacher_params
+      params.require(:teacher).permit(:name, :subj_speclization, :mobile)
+    end
+
+  
 end
